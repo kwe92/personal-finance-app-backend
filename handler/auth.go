636069f17c8a786b273c67567aac32a81060e859
Kwe <Kweayon@gmail.com>
2026-07-28 23:09:30 -0500

    firebase auth handler

---
+++ handler/auth.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"personal_finance_backend/auth"
+	"personal_finance_backend/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+type VerifyFirebaseUserRequest struct {
+	IDToken string `json:"idToken" binding:"required"`
+}
+
+func VerifyFirebaseUser(c *gin.Context) {
+	var payload VerifyFirebaseUserRequest
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := auth.VerifyIDToken(context.Background(), payload.IDToken)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid firebase token"})
+		return
+	}
+
+	database.DefaultStore.SaveUser(database.UserRecord{FirebaseUID: user.UID, Email: user.Email})
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "firebase user verified",
+		"uid":     user.UID,
+		"email":   user.Email,
+	})
+}

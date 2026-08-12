00b9454bc0a3db0894ac56eafa4a16ffc00e0ab2
Kwe <Kweayon@gmail.com>
2026-08-12 17:26:20 -0500

    injected dependencies

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"personal_finance_backend/database"
 	"personal_finance_backend/loader"
+	"personal_finance_backend/plaid_config"
 	"personal_finance_backend/router"
 )
 
 func main() {
 	loader.LoadEnv()
 	loader.InitialConfigs()
-	r := router.SetupRouter()
+	r := router.SetupRouter(plaid_config.Client, *database.DefaultStore)
 
 	if err := r.Run(":" + loader.GetPort()); err != nil {
 		panic("unable to start server: " + err.Error())
